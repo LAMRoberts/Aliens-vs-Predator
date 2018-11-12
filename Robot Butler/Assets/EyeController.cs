d9993a93692248105343b30013a11cf028124ff6
Luke <luke4.roberts@live.uwe.ac.uk>
2018-11-12 17:17:25 +0000

    Unity Project Upload

---
+++ Robot Butler/Assets/EyeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeController : MonoBehaviour
+{
+    public Material eyeColor;
+
+    private Renderer r;
+    private Light l;
+
+    private float range;
+    private bool increase = true;
+    public float speed = 0.0f;
+
+	void Start ()
+    {
+        r = GetComponent<Renderer>();
+        r.material = eyeColor;
+
+        l = GetComponentInChildren<Light>();
+        l.color = eyeColor.color;
+	}
+
+	void Update ()
+    {
+        if (range > 0.8f)
+        {
+            increase = false;
+        }
+        else if (range < 0.05f)
+        {
+            increase = true;
+        }
+
+        if (increase)
+        {
+            range += Time.deltaTime * speed;
+        }
+        else
+        {
+            range -= Time.deltaTime * speed;
+        }
+        
+        l.range = range;
+	}
+}
