64bf62f49d8d20bee44b6a5e9d426bc5f352c7a4
Huw Tingley <Huw.Tingley@gmail.com>
2018-11-30 16:59:42 +0000

    Ident Mockup started

---
+++ Robot Butler/Assets/MoveUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUI : MonoBehaviour
+{
+    [SerializeField]
+    private float TimeTillMoveStart;
+
+    [SerializeField]
+    private Transform TargetTransform;
+
+    [SerializeField]
+    private float TimeToTarget;
+
+    private float TotalTime = 0;
+
+    private bool MoveSpeedSet = false;
+    private float MoveSpeedPerSec;
+
+	void Update ()
+	{
+	    TotalTime += Time.deltaTime;
+
+	    if (TotalTime > TimeTillMoveStart && TotalTime < (TimeTillMoveStart + TimeToTarget))
+	    {
+	        if (!MoveSpeedSet)
+	        {
+	            CalcMoveSpeed();
+	        }
+
+	        GetComponent<RectTransform>().position =
+	            Vector3.MoveTowards(GetComponent<RectTransform>().position, TargetTransform.position,
+	                MoveSpeedPerSec * Time.deltaTime);
+	    }
+	}
+
+    void CalcMoveSpeed()
+    {
+        MoveSpeedSet = true;
+        MoveSpeedPerSec = Vector3.Distance(transform.position, TargetTransform.position) / TimeToTarget;
+        Debug.Log(TargetTransform.gameObject.name + ": " + MoveSpeedPerSec);
+    }
+}
---
+++ Robot Butler/Assets/TextFade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour
+{
+    [SerializeField]
+    private float TimeTillFadeStart;
+
+    [SerializeField]
+    private float TimeTakenToFade;
+
+    private float TotalTime = 0;
+
+    private float AlphaFadePerSec;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    AlphaFadePerSec = 1 / TimeTakenToFade;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    TotalTime += Time.deltaTime;
+	    if (TotalTime > TimeTillFadeStart)
+	    {
+            GetComponent<Text>().color -= new Color(0, 0, 0, AlphaFadePerSec * Time.deltaTime);
+	    }
+
+	}
+}
