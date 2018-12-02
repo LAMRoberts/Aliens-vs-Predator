43b82c02426b36da51774427f97ed09062f6e180
Huw Tingley <huw.tingley@gmail.com>
2018-12-02 14:28:43 +0000

    adding particle effects from asset store

---
+++ Robot Butler/Assets/PlayParticleEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticleEffect : MonoBehaviour {
+
+    [SerializeField]
+    private float TimeTillParticleSpawn;
+
+    private float TimePassed = 0;
+    private bool has_played = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TimePassed += Time.deltaTime;
+
+		if((TimePassed > TimeTillParticleSpawn) && !has_played)
+        {
+            has_played = true;
+            GetComponent<ParticleSystem>().Play();
+        }
+	}
+}
