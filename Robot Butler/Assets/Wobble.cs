32abeee63a08025d94701c5b6f617add4ccf2f92
Huw Tingley <huw.tingley@gmail.com>
2019-02-12 16:41:35 +0000

    Breaking robot added

---
+++ Robot Butler/Assets/Wobble.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wobble : MonoBehaviour
+{
+    [SerializeField]
+    private float wobble_z_amout;
+    [SerializeField]
+    private float wobble_z_interval;
+    private bool flip_z = false;
+    private float z_time = 0;
+    private bool z_initial = true;
+
+    void Update()
+    {
+        z_time += Time.deltaTime;
+
+        //z wobble
+        if (z_time > wobble_z_interval)
+        {
+            flip_z = !flip_z;
+            z_time = 0;
+            if (z_initial)
+            {
+                z_initial = false;
+                wobble_z_amout *= 2;
+            }
+        }
+    }
+}
