dee00241db6e6f696098ee1f716ebbb8a9705a93
Luke <luke4.roberts@live.uwe.ac.uk>
2019-02-22 14:40:53 +0000

    checklist excel and world scene stuff

---
+++ Robot Butler/Assets/CityscapeCameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityscapeCameraMovement : MonoBehaviour
+{
+    public List<Transform> positions;
+    public float distance = 0.0f;
+    public float positionSpeed = 0.0f;
+    public float rotationSpeed = 0.0f;
+    private Transform target;
+    private int t = 0;
+    private bool go = false;
+
+    private void Start()
+    {
+        target = positions[t];
+
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey("c"))
+        {
+            go = true;
+        }
+
+		if (go)
+        {
+            if (Vector3.Distance(transform.position, target.position) > distance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, positionSpeed);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, rotationSpeed);
+
+                Debug.Log("moving");
+            }
+            else
+            {
+                t++;
+                target = positions[t];
+
+                Debug.Log(target);
+            }
+        }
+	}
+}
---
+++ Robot Butler/Assets/JunctionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunctionController : MonoBehaviour
+{
+    public Transform upTrafficStopper;
+    public Transform downTrafficStopper;
+    public Transform leftTrafficStopper;
+    public Transform rightTrafficStopper;
+
+    public List<Transform> upCrossings;
+    public List<Transform> downCrossings;
+    
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
