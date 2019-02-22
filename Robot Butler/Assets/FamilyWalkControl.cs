c40906e1de7da1f508ed7dd89d226401e3c88142
Unknown <bryn2.findlay-dykes@live.uwe.ac.uk>
2019-02-22 17:13:02 +0000

    family prefab updates

---
+++ Robot Butler/Assets/FamilyWalkControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamilyWalkControl : MonoBehaviour
+{
+    bool walk = false;
+	// Use this for initialization
+	void Start () {
+        GetComponent<FamilyWalkControl>().walk = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            walk = !walk;
+            GetComponent<Animator>().SetBool("Walk", walk);
+            GetComponent<HumanAnimer>().forceWalk = walk;
+        }
+	}
+}
