2475dd901ded972ce1cd5ada82b28235135ffbdc
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2019-03-05 16:19:31 +0000

    New robot picking up magazines and terrain added to house interior

---
+++ Robot Butler/Assets/NewRobotMagazineEvents.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewRobotMagazineEvents : MonoBehaviour
+{
+    public Transform[] magazines;
+    private int activeMagazine = 0;
+
+    public Transform leftAnchor;
+    public Transform rightAnchor;
+
+	public void PickUp()
+    {
+        Transform mag = magazines[activeMagazine];
+        mag.parent = rightAnchor;
+        mag.localPosition = Vector3.zero;
+        mag.localEulerAngles = Vector3.zero;
+    }
+
+    public void SwapHands()
+    {
+        Transform mag = magazines[activeMagazine];
+        mag.parent = leftAnchor;
+        mag.localPosition = Vector3.zero;
+        mag.localEulerAngles = Vector3.zero;
+    }
+
+    public void Drop()
+    {
+        Transform mag = magazines[activeMagazine];
+        mag.parent = null;
+        mag.gameObject.AddComponent<Rigidbody>();
+
+        activeMagazine++;
+    }
+}
