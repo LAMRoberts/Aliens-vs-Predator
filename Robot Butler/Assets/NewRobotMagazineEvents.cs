3b09e4c5249e62ca57535b1db74abb4b7b4a7eaf
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2019-03-05 18:04:37 +0000

    New robot in house animations done

--- Robot Butler/Assets/NewRobotMagazineEvents.cs
+++ Robot Butler/Assets/NewRobotMagazineEvents.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class NewRobotMagazineEvents : MonoBehaviour
 {
+    private enum State
+    {
+        MAGAZINE, MOVING, HOMEWORK
+    }
+    private State state = State.MAGAZINE;
+
+    private Animation anim;
+
+
+    public Transform homework;
     public Transform[] magazines;
     private int activeMagazine = 0;
 
     public Transform leftAnchor;
     public Transform rightAnchor;
 
-	public void PickUp()
+    private bool isHomework = false;
+    private bool destReached = false;
+    private bool animPlayed = false;
+
+    private void Start()
     {
-        Transform mag = magazines[activeMagazine];
+        anim = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.MAGAZINE:
+                if (!anim.isPlaying && activeMagazine < magazines.Length)
+                    anim.Play("NewRobotMagazine");
+                else if (!anim.isPlaying && activeMagazine >= magazines.Length)
+                    state = State.MOVING;
+                break;
+            case State.MOVING:
+                isHomework = true;
+                if (!anim.isPlaying && !destReached)
+                {
+                    anim.Play("NewRobotWalk");
+                    destReached = true;
+                }
+                else if (!anim.isPlaying && destReached)
+                    state = State.HOMEWORK;
+                break;
+            case State.HOMEWORK:
+                if (!animPlayed)
+                {
+                    anim.Play("NewRobotHomework");
+                    animPlayed = true;
+                }
+                break;
+        }
+    }
+
+
+
+    public void PickUp()
+    {
+        Transform mag = homework;
+
+        if (!isHomework)
+            mag = magazines[activeMagazine];
+
+
         mag.parent = rightAnchor;
         mag.localPosition = Vector3.zero;
         mag.localEulerAngles = Vector3.zero;
