a66e77eadc2d3eb82689f33e1708b376338022e3
Nathan <nathan.butt@gmail.com>
2019-03-08 18:29:46 +0000

    Created head breakdown and added setup for improved head breakdown.

---
+++ Robot Butler/Assets/Scripts/RotateGameObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGameObject : MonoBehaviour
+{
+    public bool worldAxis = true;
+    public Vector3 axes = Vector3.up;
+
+    public float speed = 1;
+
+    private void Start()
+    {
+        if(!worldAxis) { axes = transform.up; }
+    }
+
+    void Update ()
+    {
+        transform.Rotate(axes * (speed * Time.deltaTime),Space.Self);
+    }
+}
