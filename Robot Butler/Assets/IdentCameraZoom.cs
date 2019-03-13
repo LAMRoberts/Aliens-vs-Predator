5216f387536d0b4a1768fb3903a34ba55351445c
Luke Roberts <luke.am.roberts@gmail.com>
2019-03-13 01:23:40 +0000

    ident rough cut 2 + FREE video

---
+++ Robot Butler/Assets/IdentCameraZoom.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdentCameraZoom : MonoBehaviour
+{
+    public Material mat;
+
+    Camera cam = null;
+
+    public float speed = 0.0f;
+
+    public float aSpeed = 0.0f;
+
+    private bool zoom = false;
+
+    private bool albedo = false;
+
+    private float time = 0.0f;
+
+    public float maxTime = 0.0f;
+
+    private void Start()
+    {
+       cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    public void StartZoom()
+    {
+        zoom = true;
+
+        mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, 0.0f);
+    }
+
+    void Update ()
+    {
+		if (Input.GetKeyDown("h"))
+        {
+            zoom = true;
+        }
+
+        if (zoom)
+        {
+            Zoom();
+
+            Debug.Log("zooming");
+
+            time += Time.deltaTime;
+
+            if (time >= maxTime)
+            {
+                zoom = false;
+
+                albedo = true;
+            }
+        }
+
+        if (albedo)
+        {
+            Albedo();
+        }
+	}
+
+    private void Zoom()
+    {
+        cam.fieldOfView = cam.fieldOfView + speed;
+    }
+
+    private void Albedo()
+    {
+        if (mat.color.a < 1.0f)
+        {
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a + aSpeed);
+        }
+    }
+}
