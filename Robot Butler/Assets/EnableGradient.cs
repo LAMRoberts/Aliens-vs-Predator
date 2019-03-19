99bdaf76834672c27a15cfc280915a07fa3850c7
Luke <luke4.roberts@live.uwe.ac.uk>
2019-03-19 16:53:30 +0000

    Jazzing up the ident

---
+++ Robot Butler/Assets/EnableGradient.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGradient : MonoBehaviour
+{
+    public GameObject gradient;
+    public GameObject sentientE;
+
+	void Start ()
+    {
+        StartCoroutine(_Enable());
+	}
+
+    IEnumerator _Enable()
+    {
+        yield return new WaitForSeconds(6.0f);
+
+        gradient.SetActive(true);
+        sentientE.SetActive(true);
+    }
+}
