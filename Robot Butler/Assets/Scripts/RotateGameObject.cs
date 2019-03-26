23244e73f09c53cc9ddb3e8ed62010193c600d6d
Huw Tingley <huw.tingley@gmail.com>
2019-03-26 11:59:27 +0000

    Updated Narrator script, redid cpu anim, test trail added

--- Robot Butler/Assets/Scripts/RotateGameObject.cs
+++ Robot Butler/Assets/Scripts/RotateGameObject.cs
@@ -9,6 +9,16 @@ public class RotateGameObject : MonoBehaviour
 
     public float speed = 1;
 
+    [SerializeField]
+    private bool timer = false;
+
+    [SerializeField]
+    private float start_after_secs = 0;
+    [SerializeField]
+    private float stop_after_secs = 100;
+
+    private float time_passed = 0;
+
     private void Start()
     {
         if(!worldAxis) { axes = transform.up; }
@@ -16,6 +26,17 @@ public class RotateGameObject : MonoBehaviour
 
     void Update ()
     {
-        transform.Rotate(axes * (speed * Time.deltaTime),Space.Self);
+        if (timer)
+        {
+            time_passed += Time.deltaTime;
+            if ((time_passed > start_after_secs) && (time_passed < stop_after_secs))
+            {
+                transform.Rotate(axes * (speed * Time.deltaTime), Space.Self);
+            }
+        }
+        else
+        {
+            transform.Rotate(axes * (speed * Time.deltaTime), Space.Self);
+        }
     }
 }
