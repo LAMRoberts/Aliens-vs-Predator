6ac0bdb7009b81e418592bed4f3c586bcfedcc25
Luke <luke4.roberts@live.uwe.ac.uk>
2019-03-29 16:28:34 +0000

    ident cut 4

---
+++ Robot Butler/Assets/IdentFadeIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdentFadeIn : MonoBehaviour
+{
+    public Material mat;
+    public float delay = 0.0f;
+    public float speed = 1.0f;
+
+    private void Start()
+    {
+        delay += 1.0f;
+    }
+
+    void Update ()
+    {
+        if (delay > 0.0f)
+        {
+            delay -= speed * Time.deltaTime;
+        }
+
+        mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, delay);
+    }
+}
